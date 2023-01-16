de3be22d5ec2957ece69f46119197c19c91a22a1
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2023-01-16 03:32:23 -0300

    fix: trying to fix 'Error: connect ECONNREFUSED 127.0.0.1:8811' on github actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,11 +5,17 @@ export default defineConfig({
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
       const port = process.env.PORT ?? (isDev ? "3000" : "8811");
-      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
-        baseUrl: `http://localhost:${port}`,
-        video: !process.env.CI,
-        screenshotOnRunFailure: !process.env.CI,
-      };
+      const configOverrides: Partial<Cypress.PluginConfigOptions> = isDev
+        ? {
+            baseUrl: `http://localhost:${port}`,
+            video: !process.env.CI,
+            screenshotOnRunFailure: !process.env.CI,
+          }
+        : {
+            baseUrl: `http://prisma:${port}`,
+            video: !process.env.CI,
+            screenshotOnRunFailure: !process.env.CI,
+          };
 
       // To use this:
       // cy.task('log', whateverYouWantInTheTerminal)
